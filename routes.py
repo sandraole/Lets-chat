af3d5d8f3042f8c7357ac833c155eeff1c61cb94
Sandra Ole <sandra.ole@hotmail.fi>
2024-04-23 22:05:25 +0300

    Added flash notifications and new routes: platform and rules.

--- routes.py
+++ routes.py
@@ -1,12 +1,13 @@
 from app import app
-from flask import redirect, render_template, request
-import users, visits
+from flask import redirect, render_template, request, flash
+import users, visits, messages
 
 @app.route('/')
 def index():
     visits.add_visit()
     counter = visits.get_counter()
-    return render_template('index.html', counter=counter)
+    list = messages.get_list()
+    return render_template('index.html', counter=counter, count=len(list), messages=list)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -18,7 +19,7 @@ def login():
         password = request.form['password']
 
     if not users.login(username, password):
-        return render_template('error.html', message='Wrong username or password')
+        return render_template('error.html', message='Wrong username, password or no user created')
 
     return redirect('/profile')
 
@@ -34,25 +35,30 @@ def register():
 
     if request.method == 'POST':
         username = request.form['username']
-        
-        if len(username) < 1 or len(username) > 20:
-            return render_template('error.html', message='Username must have 1-20 characters')
-
         password1 = request.form['password1']
-        password2 = request.form['password2']
+        password2 = request.form ['password2']
+        role = request.form['role']
+        
+        if len(username) < 1 or len(username) > 25:
+            flash('Username must have 1-25 characters')
+            return render_template('register.html')
 
         if password1 != password2:
-            return render_template('error.html', message='Passwords do not match')
+            flash('Passwords do not match')
+            return render_template('register.html')
         if password1 == '':
-            return render_template('error.html', message='Password is empty')
+            flash('Password is empty')
+            return render_template('register.html')
 
-        role = request.form['role']
         if role not in ('1', '2'):
-            return render_template('error.html', message='Unknown user role')
+            flash('Unknown user role')
+            return render_template('register.html')
 
         if not users.register(username, password1, role):
-            return render_template('error.html', message='Registartion failed, make sure to check username and password')
+            flash('Registartion failed, make sure to check username and password')
+            return render_template('register.html')
         
+        flash('User registration successful. You may now proceed to login.', 'success')
         return redirect('/')
     return render_template('register.html')
 
@@ -60,7 +66,21 @@ def register():
 def profile():
     return render_template('profile.html')
 
+@app.route('/send', methods=['POST'])
+def send():
+    content = request.form['content']
+    if messages.send(content):
+        return redirect('/platform')
+    else:
+        return render_template('error.html', message='Message failed to send.')
+    
 
+@app.route('/platform')
+def platform():
+    return render_template('platform.html')
 
+@app.route('/rules')
+def rules():
+    return render_template('rules.html')
 
 
